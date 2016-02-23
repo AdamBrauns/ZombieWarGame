2674c5ee20fdbd9d1ea83e49fdac46ee30916046
Tydiggidy <lindsayt10@uww.edu>
2016-02-23 12:50:57 -0600

    Create Predator.java

---
+++ src/zombiewar/impl/Predator.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiewar.impl;
+
+import zombiewar.intf.ISurvivor;
+import zombiewar.intf.IZombie;
+
+public class Predator extends Character implements IZombie{
+
+	/**
+	 * A Predator starts with 80 points of health
+	 */
+	public Predator() {
+		super(80);
+	}
+
+	@Override
+	public void attack(ISurvivor survivor) {
+		survivor.decreaseHealth(10);
+	}
+	
+	
+}
