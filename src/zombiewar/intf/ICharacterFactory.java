cd110ed414b0c09ac07f21d7968487edc6ae9e3d
Tydiggidy <lindsayt10@uww.edu>
2016-02-23 13:04:33 -0600

    Update ICharacterFactory.java

--- src/zombiewar/intf/ICharacterFactory.java
+++ src/zombiewar/intf/ICharacterFactory.java
@@ -1,8 +1,36 @@
 package zombiewar.intf;
+package zombiewar.impl;
 
 public interface ICharacterFactory{
  
   //Make a character. If type is soldier, return a Solider object.
-  public ICharacter make(String type);
+  public ICharacter make(String type){
+   
+    switch(type){
+     
+      case "common": return new Common();
+      break;
+      
+      case "tank": return new Tank();
+      break;
+      
+      case "predator": return new Predator();
+      break;
+      
+      case "soldier": return new Soldier();
+      break;
+      
+      case "teacher": return new Teacher();
+      break;
+      
+      case "student": return new Student();
+      break;
+      
+      case "child": return new Child();
+      break;
+      
+    }//end switch
+   
+  }//end make 
 
 }
