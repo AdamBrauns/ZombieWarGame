b07685f2a1e4712e3356a3094e2cabf9a3239361
Tydiggidy <lindsayt10@uww.edu>
2016-02-23 13:08:18 -0600

    Create Student.java

---
+++ src/zombiewar/impl/Student.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiewar.impl;
+
+import zombiewar.intf.ISurvivor;
+import zombiewar.intf.IZombie;
+
+public class Student extends Character implements ISurvivor{
+
+	/**
+	 * A student starts with 50 points of health
+	 */
+	public Student() {
+		super(50);
+	}
+
+	@Override
+	public void attack(IZombie zombie) {
+		zombie.decreaseHealth(4);
+	}
+	
+	
+}
