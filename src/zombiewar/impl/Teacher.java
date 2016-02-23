b9cc2402834d53960c06691ba0f92cfdab293d10
Tydiggidy <lindsayt10@uww.edu>
2016-02-23 12:47:33 -0600

    Create Teacher.java

---
+++ src/zombiewar/impl/Teacher.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiewar.impl;
+
+import zombiewar.intf.ISurvivor;
+import zombiewar.intf.IZombie;
+
+
+public class Teacher extends Character implements ISurvivor{
+
+	/**
+	 * A teacher starts with 50 points of health
+	 */
+	public Teacher() {
+		super(50);
+	}
+
+	@Override
+	public void attack(IZombie zombie) {
+		zombie.decreaseHealth(5);
+	}
+	
+	
+}
