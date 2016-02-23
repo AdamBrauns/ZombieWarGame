e79041d236cdce56c1c160be9b41ba2eb8c0e787
Reinaldo <rms_master@hotmail.com>
2016-02-23 14:50:48 -0600

    The factory was implemented in the Interface, which it should not be. Corrected.

--- src/zombiewar/impl/CharacterFactory.java
+++ src/zombiewar/impl/CharacterFactory.java
@@ -3,34 +3,47 @@ package zombiewar.impl;
 
 import zombiewar.intf.ICharacter;
 import zombiewar.intf.ICharacterFactory;
+
 /**
- *
  * @author thaoc
  */
-public class CharacterFactory implements ICharacterFactory{
-  
-  public static final ICharacterFactory instance = new CharacterFactory();
-  
-  private CharacterFactory(){
-   
-  }
+public class CharacterFactory implements ICharacterFactory {
 
-  /**
-   * Make the character.  To support more characters,
-   * simply implement a type and then add a case statement
-   * to this method.
-   * 
-   * @param type The type of character to make.
-   * @return 
-   */
-  
-  @Override
-  public ICharacter make(String type) {
-    switch(type){
-      case "soldier"  : return new Soldier();
-      case "tank"     : return new Tank();
-    }
-    return null;
-  }
-  
+	public static final ICharacterFactory instance = new CharacterFactory();
+
+	private CharacterFactory() {
+
+	}
+
+	/**
+	 * Make the character.  To support more characters,
+	 * simply implement a type and then add a case statement
+	 * to this method.
+	 *
+	 * @param type The type of character to make.
+	 * @return the character object, otherwise, a null object.
+	 */
+
+	@Override
+	public ICharacter make(String type) {
+		switch (type) {
+
+			case "common":
+				return new Common();
+			case "tank":
+				return new Tank();
+			case "predator":
+				return new Predator();
+			case "soldier":
+				return new Soldier();
+			case "teacher":
+				return new Teacher();
+			case "student":
+				return new Student();
+			case "child":
+				return new Child();
+		}
+		return null;
+	}
+
 }
--- src/zombiewar/intf/ICharacterFactory.java
+++ src/zombiewar/intf/ICharacterFactory.java
@@ -1,36 +1,8 @@
 package zombiewar.intf;
-package zombiewar.impl;
 
 public interface ICharacterFactory{
- 
+
   //Make a character. If type is soldier, return a Solider object.
-  public ICharacter make(String type){
-   
-    switch(type){
-     
-      case "common": return new Common();
-      break;
-      
-      case "tank": return new Tank();
-      break;
-      
-      case "predator": return new Predator();
-      break;
-      
-      case "soldier": return new Soldier();
-      break;
-      
-      case "teacher": return new Teacher();
-      break;
-      
-      case "student": return new Student();
-      break;
-      
-      case "child": return new Child();
-      break;
-      
-    }//end switch
-   
-  }//end make 
+  ICharacter make(String type);
 
 }
