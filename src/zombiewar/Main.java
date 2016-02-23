a1bbcdb08adc084f971d775c80fbfe47e1ad5886
Reinaldo <rms_master@hotmail.com>
2016-02-23 17:57:43 -0600

    Added new survivor in switch

--- src/zombiewar/Main.java
+++ src/zombiewar/Main.java
@@ -11,9 +11,46 @@ import zombiewar.intf.IZombie;
  * @author thaoc
  */
 public class Main {
-  
+
   private static final ICharacterFactory factory = CharacterFactory.instance;
-  
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+
+		IZombie[] zombies = randomZombies();
+		ISurvivor[] survivors = randomSurvivors();
+
+		System.out.println("We have " + survivors.length + " survivors trying to make it to safety.");
+		System.out.println("But there are " + zombies.length + " zombies waiting for them.");
+
+		//TODO: the survivors attack first.  One character attack each zombie.
+		//      When all the survivors have done attacking, it's the zombies'
+		//      turn to attack.  For each zombie that is still alive, attack
+		//      each suvivor that is still alive.  Repeat this cycle until
+		//      all the zombies are all dead or all the survivors are all dead.
+
+		while (true) {
+			survivorsAttack(survivors, zombies);
+			zombiesAttack(survivors, zombies);
+
+
+			if (allDead(zombies) || allDead(survivors)) { break; }
+		}
+
+		if (allDead(survivors)) {
+			System.out.println("None of the survivors made it.");
+		} else {
+			int count = 0;
+			for (int i = 0; i < survivors.length; i++) {
+				if (survivors[i].isAlive()) { count++; }
+			}
+			System.out.println("It seems " + count + " have made it to safety.");
+		}
+
+	}
+
   public static IZombie[] randomZombies() {
     int numZombies = (int) (Math.random() * 10);
     IZombie[] zombies = new IZombie[numZombies];
@@ -32,60 +69,22 @@ public class Main {
     int numZombies = (int) (Math.random() * 20);
     ISurvivor[] survivors = new ISurvivor[numZombies];
     for (int i = 0; i < survivors.length; i++) {
-      int type = (int) (Math.random() * 3);
-      switch(type){
-        case 0: survivors[i] = (ISurvivor) factory.make("soldier"); break;
-        case 1: survivors[i] = (ISurvivor) factory.make("teacher"); break;
+	    int type = (int) (Math.random() * 5);
+	    switch (type) {
+		    case 0:
+			    survivors[i] = (ISurvivor) factory.make("soldier");
+			    break;
+		    case 1: survivors[i] = (ISurvivor) factory.make("teacher"); break;
         case 2: survivors[i] = (ISurvivor) factory.make("student"); break;
-        case 3: survivors[i] = (ISurvivor) factory.make("child"); break;
+		    case 3:
+			    survivors[i] = (ISurvivor) factory.make("child");
+			    break;
+		    case 4:
+			    survivors[i] = (ISurvivor) factory.make("dog");
+			    break;
       }
     }
-    return survivors;
-  }
-
-  public static boolean allDead(ICharacter[] characters){
-    boolean allDead = true;
-    for(int i=0; i<characters.length; i++){
-      allDead &= !characters[i].isAlive();
-    }
-    return allDead;
-  }
-
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-
-	  IZombie[] zombies = randomZombies();
-	  ISurvivor[] survivors = randomSurvivors();
-
-	  System.out.println("We have " + survivors.length + " survivors trying to make it to safety.");
-	  System.out.println("But there are " + zombies.length + " zombies waiting for them.");
-
-	  //TODO: the survivors attack first.  One character attack each zombie.
-	  //      When all the survivors have done attacking, it's the zombies'
-	  //      turn to attack.  For each zombie that is still alive, attack
-	  //      each suvivor that is still alive.  Repeat this cycle until
-	  //      all the zombies are all dead or all the survivors are all dead.
-
-	  while (true) {
-		  survivorsAttack(survivors, zombies);
-		  zombiesAttack(survivors, zombies);
-
-
-		  if ( allDead(zombies) || allDead(survivors) ) break;
-	  }
-
-	  if (allDead(survivors)) {
-		  System.out.println("None of the survivors made it.");
-	  } else {
-		  int count = 0;
-		  for (int i = 0; i < survivors.length; i++) {
-			  if (survivors[i].isAlive()) count++;
-		  }
-		  System.out.println("It seems " + count + " have made it to safety.");
-	  }
-
+	  return survivors;
   }
 
 	public static void survivorsAttack( ISurvivor[] survivors, IZombie[] zombies ){
@@ -116,6 +115,14 @@ public class Main {
 				}
 			}
 		}
+	}
+
+	public static boolean allDead(ICharacter[] characters) {
+		boolean allDead = true;
+		for (int i = 0; i < characters.length; i++) {
+			allDead &= !characters[i].isAlive();
+		}
+		return allDead;
 	}
 
 
