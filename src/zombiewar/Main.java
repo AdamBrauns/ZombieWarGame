890f39858c679f6a9927aed60c2d76e4ca4a0a4a
Reinaldo <rms_master@hotmail.com>
2016-02-23 21:07:40 -0600

    Added more zombies because survivors always survive. :)

--- src/zombiewar/Main.java
+++ src/zombiewar/Main.java
@@ -55,7 +55,7 @@ public class Main {
 		int numZombies = (int) (Math.random() * 10);
 		IZombie[] zombies = new IZombie[numZombies];
 		for (int i = 0; i < zombies.length; i++) {
-			int zombieType = (int) (Math.random() * 5);
+			int zombieType = (int) (Math.random() * 10);
 			switch (zombieType) {
 				case 0:
 					zombies[i] = (IZombie) factory.make("common");
